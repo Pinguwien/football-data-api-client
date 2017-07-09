b2e7fcf06f583db3bd71b70a85ba7a9d773f25e8
Michael Ruocco <michael.ruocco@hotmail.com>
2017-07-09 08:33:50 +0100

    removes unused imports

--- src/main/java/uk/co/mruoc/footballdata/client/TokenHttpClient.java
+++ src/main/java/uk/co/mruoc/footballdata/client/TokenHttpClient.java
@@ -1,7 +1,5 @@
 package uk.co.mruoc.footballdata.client;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import uk.co.mruoc.http.client.Headers;
 import uk.co.mruoc.http.client.Response;
 import uk.co.mruoc.http.client.SimpleHttpClient;
