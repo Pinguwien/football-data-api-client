a42620aeb041b630a5ccb47b435868c32dac75d0
Michael Ruocco <michael.ruocco@hotmail.com>
2017-07-23 07:43:22 +0100

    adds player parser

---
+++ src/main/java/uk/co/mruoc/footballdata/client/PlayerParser.java
@@ -0,0 +1,62 @@
+package uk.co.mruoc.footballdata.client;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.joda.time.LocalDate;
+import uk.co.mruoc.footballdata.model.Player;
+
+import java.math.BigDecimal;
+
+public class PlayerParser {
+
+    private final JsonParser parser = new JsonParser();
+    private final JsonExtractor extractor = new JsonExtractor();
+
+    public Player toPlayer(String jsonString) {
+        JsonElement element = parser.parse(jsonString);
+        JsonObject json = element.getAsJsonObject();
+        return toPlayer(json);
+    }
+
+    public Player toPlayer(JsonObject json) {
+        return new Player.PlayerBuilder()
+                .setName(parseName(json))
+                .setPosition(parsePosition(json))
+                .setJerseyNumber(parseJerseyNumber(json))
+                .setDateOfBirth(parseDateOfBirth(json))
+                .setNationality(parseNationality(json))
+                .setContractUntil(parseContractUntil(json))
+                .setMarketValue(parseMarketValue(json))
+                .build();
+    }
+
+    private String parseName(JsonObject json) {
+        return extractor.extractString(json, "name");
+    }
+
+    private String parsePosition(JsonObject json) {
+        return extractor.extractString(json, "position");
+    }
+
+    private int parseJerseyNumber(JsonObject json) {
+        return extractor.extractInt(json, "jerseyNumber");
+    }
+
+    private LocalDate parseDateOfBirth(JsonObject json) {
+        return extractor.extractLocalDate(json, "dateOfBirth");
+    }
+
+    private String parseNationality(JsonObject json) {
+        return extractor.extractString(json, "nationality");
+    }
+
+    private LocalDate parseContractUntil(JsonObject json) {
+        return extractor.extractLocalDate(json, "contractUntil");
+    }
+
+    private BigDecimal parseMarketValue(JsonObject json) {
+        return extractor.extractBigDecimal(json, "marketValue");
+    }
+
+}
---
+++ src/test/java/uk/co/mruoc/footballdata/client/PlayerParserTest.java
@@ -0,0 +1,90 @@
+package uk.co.mruoc.footballdata.client;
+
+import org.joda.time.LocalDate;
+import org.junit.Test;
+import uk.co.mruoc.footballdata.model.Player;
+import uk.co.mruoc.footballdata.model.Team;
+import uk.co.mruoc.properties.ClasspathFileContentLoader;
+import uk.co.mruoc.properties.FileContentLoader;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlayerParserTest {
+
+    private static final String JSON_FILE_PATH = "/player.json";
+
+    private final FileContentLoader loader = new ClasspathFileContentLoader();
+    private final PlayerParser parser = new PlayerParser();
+
+    @Test
+    public void shouldParseName() {
+        String json = loader.loadContent(JSON_FILE_PATH);
+
+        Player player = parser.toPlayer(json);
+
+        assertThat(player.getName()).isEqualTo("Petr Cech");
+    }
+
+    @Test
+    public void shouldParsePosition() {
+        String json = loader.loadContent(JSON_FILE_PATH);
+
+        Player player = parser.toPlayer(json);
+
+        assertThat(player.getPosition()).isEqualTo("Keeper");
+    }
+
+    @Test
+    public void shouldParseJerseyNumber() {
+        String json = loader.loadContent(JSON_FILE_PATH);
+
+        Player player = parser.toPlayer(json);
+
+        assertThat(player.getJerseyNumber()).isEqualTo(33);
+    }
+
+    @Test
+    public void shouldParseDateOfBirth() {
+        String json = loader.loadContent(JSON_FILE_PATH);
+
+        Player player = parser.toPlayer(json);
+
+        assertThat(player.getDateOfBirth()).isEqualTo(new LocalDate()
+                .withDayOfMonth(20)
+                .withMonthOfYear(5)
+                .withYear(1982));
+    }
+
+    @Test
+    public void shouldParseNationality() {
+        String json = loader.loadContent(JSON_FILE_PATH);
+
+        Player player = parser.toPlayer(json);
+
+        assertThat(player.getNationality()).isEqualTo("Czech Republic");
+    }
+
+    @Test
+    public void shouldParseContractUntil() {
+        String json = loader.loadContent(JSON_FILE_PATH);
+
+        Player player = parser.toPlayer(json);
+
+        assertThat(player.getContractUntil()).isEqualTo(new LocalDate()
+                .withDayOfMonth(30)
+                .withMonthOfYear(6)
+                .withYear(2019));
+    }
+
+    @Test
+    public void shouldParseMarketValue() {
+        String json = loader.loadContent(JSON_FILE_PATH);
+
+        Player player = parser.toPlayer(json);
+
+        assertThat(player.getMarketValue()).isEqualTo(BigDecimal.ZERO);
+    }
+
+}
