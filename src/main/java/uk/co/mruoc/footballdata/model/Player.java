b67e3b0fe5e92717650c535da1d360035e849923
Michael Ruocco <michael.ruocco@hotmail.com>
2017-07-23 07:23:01 +0100

    completes player model class

--- src/main/java/uk/co/mruoc/footballdata/model/Player.java
+++ src/main/java/uk/co/mruoc/footballdata/model/Player.java
@@ -1,35 +1,99 @@
 package uk.co.mruoc.footballdata.model;
 
+import org.joda.time.LocalDate;
+
+import java.math.BigDecimal;
+
 public class Player {
 
-    private final int id;
     private final String name;
+    private final String position;
+    private final int jerseyNumber;
+    private final LocalDate dateOfBirth;
+    private final String nationality;
+    private final LocalDate contractUntil;
+    private final BigDecimal marketValue;
 
     private Player(PlayerBuilder builder) {
-        this.id = builder.id;
         this.name = builder.name;
-    }
-
-    public int getId() {
-        return id;
+        this.position = builder.position;
+        this.jerseyNumber = builder.jerseyNumber;
+        this.dateOfBirth = builder.dateOfBirth;
+        this.nationality = builder.nationality;
+        this.contractUntil = builder.contractUntil;
+        this.marketValue = builder.marketValue;
     }
 
     public String getName() {
         return name;
     }
 
+    public String getPosition() {
+        return position;
+    }
+
+    public int getJerseyNumber() {
+        return jerseyNumber;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public LocalDate getContractUntil() {
+        return contractUntil;
+    }
+
+    public BigDecimal getMarketValue() {
+        return marketValue;
+    }
+
     public static class PlayerBuilder {
 
-        private int id;
         private String name;
-
-        public PlayerBuilder setId(int id) {
-            this.id = id;
-            return this;
-        }
+        private String position;
+        private int jerseyNumber;
+        private LocalDate dateOfBirth;
+        private String nationality;
+        private LocalDate contractUntil;
+        private BigDecimal marketValue;
 
         public PlayerBuilder setName(String name) {
             this.name = name;
+            return this;
+        }
+
+        public PlayerBuilder setPosition(String position) {
+            this.position = position;
+            return this;
+        }
+
+        public PlayerBuilder setJerseyNumber(int jerseyNumber) {
+            this.jerseyNumber = jerseyNumber;
+            return this;
+        }
+
+        public PlayerBuilder setDateOfBirth(LocalDate dateOfBirth) {
+            this.dateOfBirth = dateOfBirth;
+            return this;
+        }
+
+        public PlayerBuilder setNationality(String nationality) {
+            this.nationality = nationality;
+            return this;
+        }
+
+        public PlayerBuilder setContractUntil(LocalDate contractUntil) {
+            this.contractUntil = contractUntil;
+            return this;
+        }
+
+        public PlayerBuilder setMarketValue(BigDecimal marketValue) {
+            this.marketValue = marketValue;
             return this;
         }
 
--- src/test/java/uk/co/mruoc/footballdata/model/PlayerTest.java
+++ src/test/java/uk/co/mruoc/footballdata/model/PlayerTest.java
@@ -1,24 +1,16 @@
 package uk.co.mruoc.footballdata.model;
 
+import org.joda.time.LocalDate;
 import org.junit.Test;
 import uk.co.mruoc.footballdata.model.Player.PlayerBuilder;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlayerTest {
 
     private final PlayerBuilder builder = new PlayerBuilder();
-
-    @Test
-    public void shouldSetId() {
-        int id = 123;
-
-        Player player = builder
-                .setId(id)
-                .build();
-
-        assertThat(player.getId()).isEqualTo(id);
-    }
 
     @Test
     public void shouldSetName() {
@@ -29,6 +21,72 @@ public class PlayerTest {
                 .build();
 
         assertThat(player.getName()).isEqualTo(name);
+    }
+
+    @Test
+    public void shouldSetPosition() {
+        String position = "Defender";
+
+        Player player = builder
+                .setPosition(position)
+                .build();
+
+        assertThat(player.getPosition()).isEqualTo(position);
+    }
+
+    @Test
+    public void shouldSetJerseyNumber() {
+        int jerseyNumber = 5;
+
+        Player player = builder
+                .setJerseyNumber(jerseyNumber)
+                .build();
+
+        assertThat(player.getJerseyNumber()).isEqualTo(jerseyNumber);
+    }
+
+    @Test
+    public void shouldSetDateOfBirth() {
+        LocalDate dateOfBirth = LocalDate.now();
+
+        Player player = builder
+                .setDateOfBirth(dateOfBirth)
+                .build();
+
+        assertThat(player.getDateOfBirth()).isEqualTo(dateOfBirth);
+    }
+
+    @Test
+    public void shouldSetNationality() {
+        String nationality = "Italian";
+
+        Player player = builder
+                .setNationality(nationality)
+                .build();
+
+        assertThat(player.getNationality()).isEqualTo(nationality);
+    }
+
+    @Test
+    public void shouldSetContractUntil() {
+        LocalDate contractUntil = LocalDate.now();
+
+        Player player = builder
+                .setContractUntil(contractUntil)
+                .build();
+
+        assertThat(player.getContractUntil()).isEqualTo(contractUntil);
+    }
+
+    @Test
+    public void shouldSetMarketValue() {
+        BigDecimal marketValue = BigDecimal.valueOf(5000000);
+
+        Player player = builder
+                .setMarketValue(marketValue)
+                .build();
+
+        assertThat(player.getMarketValue()).isEqualTo(marketValue);
     }
 
 }
